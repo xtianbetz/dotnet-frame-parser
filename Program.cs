ccc654e9df05c92425aa6e62be3127ccdd841649
Christian Betz <christian.betz@gmail.com>
2021-08-30 14:07:21 -0400

    frame parsing with multibyte tags

--- Program.cs
+++ Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection.Metadata;
 
 #nullable enable
 namespace dotnet_frame_parser
 {
+    class Util
+    {
+        public static string ByteArrayToHexString(byte[] byteArray, int len)
+        {
+            var s = "";
+            for (var i = 0; i < len; i++)
+            {
+                s += String.Format("0x{0:X2}", byteArray[i]);
+                if (i < len - 1)
+                {
+                    s += " ";
+                }
+            }
+
+            return s;
+        }
+    }
+
     interface IStreamProtocolParser<TM>
     {
         public FrameParseResult<TM> ReadFramesFromBuffer(byte[] bufSlice);
@@ -16,7 +35,7 @@ namespace dotnet_frame_parser
     }
     class TagLengthValueMessage
     {
-        public byte Tag = 0;
+        public byte[] Tag = new byte[] {0xDA, 0xBB, 0xAD, 0x00};
         public byte Length = 0;
         public byte[] Value = new byte[1024];
     }
@@ -32,10 +51,20 @@ namespace dotnet_frame_parser
         public TagLengthValueProtocolScannerState ScannerState = TagLengthValueProtocolScannerState.ExpectingTag;
         public TagLengthValueMessage CurrentFrame = new TagLengthValueMessage();
         public int ValueBytesRead = 0;
+        public int TagBytesRead = 0;
+        public byte[] TagCandidate = new byte[4];
     }
+
     class TagLengthValueProtocolParser : IStreamProtocolParser<TagLengthValueMessage>
     {
         public TagLengthValueProtocolParserState State = new TagLengthValueProtocolParserState();
+
+        public int TagLength = 4;
+        public List<byte[]> ValidTags = new List<byte[]>
+        {
+            new byte[] {0xDA, 0xBB, 0xAD, 0x00}
+        };
+
         public FrameParseResult<TagLengthValueMessage> ReadFramesFromBuffer(byte[] buf)
         {
            var result =  new FrameParseResult<TagLengthValueMessage>();
@@ -46,6 +75,7 @@ namespace dotnet_frame_parser
            var bufEndPosition = bufLen - 1;
            var strideLength = 1;
            var nextValueChunkStartPosition = 0;
+           var nextTagChunkStartPosition = 0;
            
            if (State.ScannerState == TagLengthValueProtocolScannerState.ExpectingMoreValueBytes)
            {
@@ -53,18 +83,56 @@ namespace dotnet_frame_parser
                strideLength = Math.Min(bufLen, bytesWeAreStillExpecting);
                position = strideLength - 1;
            }
-            
+           else if (State.ScannerState == TagLengthValueProtocolScannerState.ExpectingTag)
+           {
+               var tagBytesWeAreStillExpecting = TagLength - State.TagBytesRead;
+               strideLength = Math.Min(bufLen, tagBytesWeAreStillExpecting);
+               position = strideLength - 1;
+           }
+             
            while (true)
            {
                var bytesRemainingInBuffer = bufEndPosition - position;
                switch (State.ScannerState)
                {
                    case TagLengthValueProtocolScannerState.ExpectingTag:
-                       State.CurrentFrame.Tag = buf[position];
-                       State.ScannerState = TagLengthValueProtocolScannerState.ExpectingLength;
-                       strideLength = 1;
+                       var tagSrcStartPosition = nextTagChunkStartPosition;
+                       var tagDestStartPosition = State.TagBytesRead;
+                       Array.Copy(buf, tagSrcStartPosition, State.TagCandidate, tagDestStartPosition, strideLength);
+                       nextTagChunkStartPosition += strideLength;
+                       State.TagBytesRead += strideLength;
+                       var bytesRemainingInTag = TagLength - State.TagBytesRead; 
+                       if (bytesRemainingInTag == 0)
+                       {
+                           var tagIsGood = false;
+                           foreach (var validTag in ValidTags)
+                           {
+                               if (validTag.SequenceEqual(State.TagCandidate))
+                               {
+                                   tagIsGood = true;
+                                   break;
+                               }
+                           }
+
+                           if (tagIsGood)
+                           {
+                               State.ScannerState = TagLengthValueProtocolScannerState.ExpectingLength;
+                           }
+                           else
+                           {
+                               State.TagCandidate = new byte[TagLength];
+                               State.TagBytesRead = 0;
+                               nextTagChunkStartPosition = position + 1;
+                           }
+                           strideLength = 1;
+                       }
+                       else
+                       {
+                           strideLength = Math.Min(bytesRemainingInBuffer, bytesRemainingInTag);
+                       }
                        break;
                    case TagLengthValueProtocolScannerState.ExpectingLength:
+                       // TODO: validate length (using protocol properties MinFrameLength/MaxFrameLength)
                        State.CurrentFrame.Length = buf[position];
                        State.ScannerState = TagLengthValueProtocolScannerState.ExpectingMoreValueBytes;
                        State.ValueBytesRead = 0;
@@ -83,7 +151,11 @@ namespace dotnet_frame_parser
                            State.ScannerState = TagLengthValueProtocolScannerState.ExpectingTag;
                            result.Frames.Add(State.CurrentFrame);
                            State.CurrentFrame = new TagLengthValueMessage();
-                           strideLength = 1;
+                           State.ValueBytesRead = 0;
+                           State.TagCandidate = new byte[TagLength];
+                           State.TagBytesRead = 0;
+                           strideLength = Math.Min(bytesRemainingInBuffer, TagLength);
+                           nextTagChunkStartPosition = position + 1;
                        }
                        else
                        {
@@ -103,182 +175,67 @@ namespace dotnet_frame_parser
         }
     }
     
-    class SimpleTwoByteMessage
-    {
-        public byte Value = 0;
-    }
-
-    enum SimpleTwoByteProtocolScannerState
-    {
-        ExpectingHeader,
-        ExpectingValue,
-    }
-
-    class SimpleTwoByteProtocolParserState
-    {
-        public SimpleTwoByteProtocolScannerState ScannerState = SimpleTwoByteProtocolScannerState.ExpectingHeader;
-    }
-
-    class TwoByteProtocolParser : IStreamProtocolParser<SimpleTwoByteMessage>
-    {
-        public SimpleTwoByteProtocolParserState State = new SimpleTwoByteProtocolParserState();
-        public FrameParseResult<SimpleTwoByteMessage> ReadFramesFromBuffer(byte[] buf)
-        {
-           var result =  new FrameParseResult<SimpleTwoByteMessage>();
-           result.Frames = new List<SimpleTwoByteMessage>();
-
-           var position = 0;
-           var bufLen = buf.Length;
-           while (position < bufLen)
-           {
-               switch (State.ScannerState)
-               {
-                   case SimpleTwoByteProtocolScannerState.ExpectingHeader:
-                       if (buf[position] == 0x99)
-                       {
-                           State.ScannerState = SimpleTwoByteProtocolScannerState.ExpectingValue;
-                       }
-                       else
-                       {
-                           Console.WriteLine("Warning: Invalid/unexpected byte in stream: {0}", buf[position]);
-                       }
-                       break;
-                   case SimpleTwoByteProtocolScannerState.ExpectingValue:
-                       State.ScannerState = SimpleTwoByteProtocolScannerState.ExpectingHeader;
-                       result.Frames.Add(new SimpleTwoByteMessage() {Value = buf[position]});
-                       break;
-               }
-               position++;
-           }
-
-           return result;
-        }
-    }
-
-
     class Program
     {
-        public static void ReadAndProcessTlvFramesInPieces()
-        {
-            // Three frames of different sizes scattered across 8 buffers
-            
-            // Frame 1
-            var bufferSlice1 = new byte[]
-            {
-                0x42, 0x03, 0x1,
-            };
-                
-            var bufferSlice2 = new byte[] {
-                                 0x2, 0x3,
-            };
-           
-            // Frame 2
-            var bufferSlice3 = new byte[]
-            {
-                0x42, 0x04,
-            };
-                
-            var bufferSlice4 = new byte[] {
-                            0x1, 0x2,
-            };
-            
-            var bufferSlice5 = new byte[] {
-                                     0x3, 0x4,
-            };
-           
-            // Frame 3
-            var bufferSlice6 = new byte[] {
-                0x42, 
-            };
-            var bufferSlice7 = new byte[] {
-                     0x01, 
-            };
-            var bufferSlice8 = new byte[] {
-                          0x01, 
-            };
-
-            var parser = new TagLengthValueProtocolParser();
-            
-            ScanAndDebug(parser, bufferSlice1);
-            ScanAndDebug(parser, bufferSlice2);
-            ScanAndDebug(parser, bufferSlice3);
-            ScanAndDebug(parser, bufferSlice4);
-            ScanAndDebug(parser, bufferSlice5);
-            ScanAndDebug(parser, bufferSlice6);
-            ScanAndDebug(parser, bufferSlice7);
-            ScanAndDebug(parser, bufferSlice8);
-        }
-
-        public static string ByteArrayToHexString(byte[] byteArray, int len)
-        {
-            var s = "";
-            for (var i = 0; i < len; i++)
-            {
-                s += String.Format("{0:X}",byteArray[i]);
-            }
-
-            return s;
-        }
-        
-        public static void ScanAndDebug(TagLengthValueProtocolParser parser, byte[] bufSlice)
+        public static void ScanAndDebugResult(TagLengthValueProtocolParser parser, byte[] bufSlice)
         {
             var scanResult = parser.ReadFramesFromBuffer(bufSlice);
             foreach (var frame in scanResult.Frames)
             {
-                Console.WriteLine("Got TLV Frame: {0}", ByteArrayToHexString(frame.Value, frame.Length));
+                Console.WriteLine("Got TLV Frame, Tag = {0}, Value = {1}",
+                    Util.ByteArrayToHexString(frame.Tag, 4),
+                    Util.ByteArrayToHexString(frame.Value, frame.Length));
             }
         }
         
-        public static void ReadAndProcessTlvFramesSmashedTogether()
+        public static void ReadAndProcessTlvMultibyteTag()
         {
+            // Three frames of different sizes all with same four-byte tag
             var bufferSlice1 = new byte[]
             {
-                0x42, 0x03, 0x1, 0x2, 0x3,
-                0x42, 0x04, 0x1, 0x2, 0x3, 0x4,
-                0x42, 0x05, 0x1, 0x2, 0x3, 0x4, 0x5
+                0xDA, 0xBB, 0xAD, 0x00, 0x03, 0x1, 0x2, 0x3,
+                0xDA, 0xBB, 0xAD, 0x00, 0x04, 0x1, 0x2, 0x3, 0x4,
+                0xDA, 0xBB, 0xAD, 0x00, 0x05, 0x1, 0x2, 0x3, 0x4, 0x5
             };
 
             var parser = new TagLengthValueProtocolParser();
-            ScanAndDebug(parser, bufferSlice1);
-        }
-        
-        public static void ReadAndProcessSerialFrames()
-        {
-            var bufferSlice1 = new byte[]
-            {
-                0x99, 0x04,
-                0x22, 0x39,
-                0x99, 0x03,
-                0x99, 0x02,
-                0x99,
-            };
-            var bufferSlice2 = new byte[]
-            {
-                0x01,
-                0x99, 0x07,
-                0x99, 0x08,
-            };
+            Console.WriteLine("All at once");
+            ScanAndDebugResult(parser, bufferSlice1);
 
-            var parser = new TwoByteProtocolParser();
-            var scanResult = parser.ReadFramesFromBuffer(bufferSlice1);
-
-            foreach (var frame in scanResult.Frames)
+            Console.WriteLine("1 byte at a time");
+            ReadBufferInSteps(parser, bufferSlice1, 1);
+            
+            for (var i = 2; i <= bufferSlice1.Length; i++)
             {
-                Console.WriteLine($"Got Frame {frame.Value}");
+                Console.WriteLine("{0} bytes at a time", i);
+                ReadBufferInSteps(parser, bufferSlice1, i);
             }
+        }
 
-            var scanResult2 = parser.ReadFramesFromBuffer(bufferSlice2);
-            foreach (var frame in scanResult2.Frames)
+        public static void ReadBufferInSteps(TagLengthValueProtocolParser parser, byte[] bufSlice, int stepSize)
+        {
+            var bytesRemaining = bufSlice.Length;
+            var nextChunkStartPosition = 0;
+            while (bytesRemaining > 0)
             {
-                Console.WriteLine($"Got Frame {frame.Value}");
+                var chunk = new byte[stepSize];
+                var chunkSize = stepSize;
+                if (chunkSize > bytesRemaining)
+                {
+                    chunkSize = bytesRemaining;
+                    Array.Resize(ref chunk, chunkSize);
+                }
+                Array.Copy(bufSlice,nextChunkStartPosition,chunk,0,chunkSize);
+                bytesRemaining -= chunkSize;
+                nextChunkStartPosition += chunkSize;
+                
+                ScanAndDebugResult(parser, chunk);
             }
         }
 
         static void Main(string[] args)
         {
-            ReadAndProcessSerialFrames();
-            ReadAndProcessTlvFramesSmashedTogether();
-            ReadAndProcessTlvFramesInPieces();
+            ReadAndProcessTlvMultibyteTag();
         }
     }
 }
